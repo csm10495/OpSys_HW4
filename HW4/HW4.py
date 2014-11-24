bd7f71a9fa6802dd5d1efaeb8a2750ecf3f15087
csm10495 <csm10495@gmail.com>
2014-11-24 01:21:24 -0500

    cProcessList and defrag

--- HW4/HW4.py
+++ HW4/HW4.py
@@ -3,6 +3,74 @@
 
 import sys
 
+#global time variable
+time = 0
+
+# ---> Work on this.
+#Process List class
+class cProcessList:
+    #Constructor, makes a cProcessList Object
+    #Give it the entire input file and it will
+    #parse it and make the list of processes
+    def __init__(self, input_file):
+        pass
+        #parse the input_file
+        #save it to a List...
+        #self._list = ...
+
+    #DON'T ACCESS _list DIRECTLY, use accessors, etc..
+
+#Process class
+class Process:
+    #Constructor, makes a Process Object
+    def __init__(self, char, arrival_times, exit_times, needed_frames):
+        self.char = char                      #Char representation
+        self.needed_frames = needed_frames    #Number of frames needed each time
+        self.arrival_times = arrival_times    #Times that the process enters
+        self.exit_times = exit_times          #Times that the process exits
+
+    #returns char
+    def getChar(self):
+        return self.char
+
+    #retuns needed_frames list
+    def getNeededFrames(self):
+        return self.needed_frames
+
+    #return arrival_times list
+    def getArrivalTimes(self):
+        return self.arrival_times
+
+    #returns the next arrival time
+    #if none exists, return -1
+    def getNextArrivalTime(self):
+        if (getRemainingInstances() >= 1):
+            return self.arrival_times[0]
+        else:
+            return -1
+
+    #return exit times list
+    def getExitTimes(self):
+        return self.exit_times
+
+    #Gets the number of times this process will run from now...
+    def getRemainingInstances(self):
+        if len(self.needed_frames) == len(self.arrival_times) == len(self.exit_times):
+            return len(self.needed_frames)
+        else:
+            print "ERROR: (len(needed_frames) == len(arrival_times) == len(exit_times)) == False"
+            return -1
+
+    #pops the top of the arrival, exit, frames
+    def popTop(self):
+        if len(self.arrival_times) <= 0:
+            print "ERROR: Attempting to popTop a done process, shouldn't happen"
+            return None
+        else:
+            frames = self.needed_frames.pop(0)
+            arrival = self.arrival_times.pop(0)
+            exit = self.exit_times.pop(0)
+            return arrival, exit, frames
 
 #Memory structure with 1600 memory frames, first 80 are for the OS
 class cMem:
@@ -16,6 +84,14 @@ class cMem:
         for i in self._memory:
             str += i
         print str
+
+    #removes all fragmentation from this cMem
+    def defrag(self):
+        self._memory = filter(lambda a: a != ".", self._memory) #removes all "."s
+        while len(self._memory < 1600): #adds all extra "."s
+            self._memory.append(".")
+        if len(self._memory) > 1600:
+            print "ERROR: The defrag has resulted in more than 1600 memory frames, this should NEVER happen"
 
     #removes a given process from this cMem
     def removeProcess(self, process_char):
@@ -38,11 +114,8 @@ class cMem:
     #return True if it worked, False if it didn't
     def addProcess(self, add_method, process_char, num_frames):
         return False
+        # ---> work on this
 
-#call necessary functions to run the simulation
-def runSimulation(quiet, input_file, mode):
-    a = cMem()
-    pass
 
 #startup function
 def startUp():
@@ -65,6 +138,15 @@ def startUp():
         sys.exit(0)
 
     runSimulation(quiet, input_file, mode)
+
+
+#call necessary functions to run the simulation
+def runSimulation(quiet, input_file, mode):
+    cPL = cProcessList()
+    cM = cMem()
+    # ---> work on this
+    pass
+
 
 
 startUp() #start the simulation
